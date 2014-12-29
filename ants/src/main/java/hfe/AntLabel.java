56210f77ca7c371590c92640b1ad41258bea4fc0
feidner <a@h41.de>
2014-12-29 13:17:26 +0100

    auf gehts

---
+++ ants/src/main/java/hfe/AntLabel.java
@@ -0,0 +1,28 @@
+package hfe;
+
+import java.awt.Point;
+
+import javax.swing.JLabel;
+
+public class AntLabel extends JLabel {
+
+	private Point point;
+	private boolean home;
+
+	public AntLabel(Point point) {
+		super();
+		this.point = point;
+	}
+
+	public Point getPoint() {
+		return point;
+	}
+	
+	public boolean isHome() {
+		return home;
+	}
+	
+	public void setHome(boolean home) {
+		this.home = home;
+	}
+}
---
+++ ants/src/main/java/hfe/AntSwingUi.java
@@ -0,0 +1,92 @@
+package hfe;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class AntSwingUi {
+
+    private JFrame f;
+    private AntService service;
+    
+
+    public static void main(String[] args) throws Exception {
+        AntSwingUi ui = new AntSwingUi();
+        ui.start();  
+    }
+    
+    public AntSwingUi() {
+    	service = new AntService();
+    	service.initGrid(300, 50);
+	}
+    
+    private void start() throws InterruptedException {
+    	createFrame();
+        while(true) {
+            Thread.sleep(100);
+            update();
+        }
+    }
+
+    void update() {
+    	service.update();
+        updateUi();
+    }
+    
+    private void updateUi() {
+    	for(Component component : f.getContentPane().getComponents()) {
+        	AntLabel label = (AntLabel)component;
+            label.setText("");            
+            label.setBackground(service.containsFoodPoint(label.getPoint()) ? Color.red : null);
+            if(label.isHome()) {
+            	label.setBackground(Color.blue);
+            }
+            service.getPoints().forEach((point)-> {
+            	if(label.getPoint().equals(point)) {
+            		label.setText("o");
+            	} 
+            });     
+        }
+    }
+
+    private void createFrame() {
+        f = new JFrame("Ants");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setContentPane(createContentPane(AntService.GRID));
+        f.setSize(1024, 860);
+        f.setVisible(true);
+    }
+
+    private Container createContentPane(int dimension) {
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(dimension, dimension));
+        for(int i = 0; i < dimension; i++) {
+            for(int j = 0; j < dimension; j++) {
+                AntLabel label = new AntLabel(new Point(i,j));
+                if(i == dimension/2 && j == dimension/2) {
+                	label.setHome(true);
+                }
+                label.setOpaque(true);              
+                p.add(label);        
+                label.addMouseListener(new MouseAdapter() {
+                	@Override
+                	public void mouseClicked(MouseEvent e) {
+                		AntLabel l = (AntLabel)e.getSource();
+                		service.addOrRemoveFoodPoint(l.getPoint());
+                	}
+				});
+            }
+        }
+        return p;
+    }
+    
+    
+ 
+}
